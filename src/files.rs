8c80b5878bd6ef50038ea533630619f55f326426
EH1121 <95305663+EH1121@users.noreply.github.com>
2022-11-12 18:26:37 +0700

    files/leaderboards/opt/topic_csv

--- src/files.rs
+++ src/files.rs
@@ -52,13 +52,15 @@ fn parse_topic(string: &str) -> Result<(u64, String, String, String, String), St
 /// Parses input from string and adds it to Topics
 fn parse_topics(topics: String, verbose: bool) -> topics_csv::Topics {
     let mut tpcs = topics_csv::Topics::new();
+    let mut curr_line = 1;
     for topic in topics.split('\n') {
         if !topic.is_empty() {
             match parse_topic(topic) {
                 Ok(tpc) => tpcs.add_new_topic(tpc.0, &tpc.1, &tpc.2, &tpc.3, &tpc.4),
-                Err(err) => if verbose { println!("Error in parsing lines in topics.csv: {err}") }
+                Err(err) => if verbose { println!("Error in parsing lines in topics.csv on line {curr_line}: {err}") }
             }
         }
+        curr_line = curr_line + 1;
     }
     tpcs
 }
@@ -82,17 +84,17 @@ fn parse_quiz(string: &str) -> Result<(u64, String, String, Choices, Vec<String>
             Ok(v) => v,
             Err(_) => return Err("ParseIntError".to_string())
         },
-        None => return Err("Missing topic_id".to_string())
+        None => return Err("Missing quiz id".to_string())
     };
 
     let quiz_name = match strings.get(1).filter(|str| !str.is_empty()) {
         Some(str) => str.to_string(),
-        None => return Err("Missing leaderboard_name".to_string())
+        None => return Err("Missing quiz name".to_string())
     };
 
     let quiz_description = match strings.get(2).filter(|str| !str.is_empty()) {
         Some(str) => str.to_string(),
-        None => return Err("Missing file_name".to_string())
+        None => return Err("Missing description".to_string())
     };
 
     let quiz_answer = match strings.get(3).filter(|str| !str.is_empty()) {
@@ -102,26 +104,28 @@ fn parse_quiz(string: &str) -> Result<(u64, String, String, Choices, Vec<String>
                 Err(_) => return Err("Error parsing answer".to_string())
             }
         },
-        None => return Err("Missing topic_name".to_string())
+        None => return Err("Missing quiz answer".to_string())
     };
 
     let quiz_choices = match strings.get(4).filter(|str| !str.is_empty()) {
         Some(str) => split_str_to_vec(str, '|'),
-        None => return Err("Missing topic_description".to_string())
+        None => return Err("Missing quiz choices".to_string())
     };
 
     Ok(( quiz_id, quiz_name, quiz_description, quiz_answer, quiz_choices))
 }
 
-pub fn parse_quizzes(quizzes: String, verbose: bool) -> Quizzes { //-> Quizzes{
+pub fn parse_quizzes(quizzes: String, filename: &str, verbose: bool) -> Quizzes { //-> Quizzes{
     let mut qz = Quizzes::new();
+    let mut curr_line = 1;
     for q in quizzes.split('\n') {
         if !q.is_empty() {
             match parse_quiz(q) {
                 Ok(quiz) => qz.add(quiz.0, &quiz.1, &quiz.2, quiz.3, quiz.4),
-                Err(err) => if verbose { println!("Error in parsing lines in topics.csv: {err}") }
+                Err(err) => if verbose { println!("Error in parsing lines in {filename} on line {curr_line}: {err}",) }
             }
         }
+        curr_line = curr_line + 1;
     }
     qz
 }
@@ -137,7 +141,7 @@ pub fn load_quizzes(topic: Topic, verbose: bool) -> std::io::Result<Quizzes> { /
     let mut file = File::open(path)?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
-    Ok(parse_quizzes(buffer, verbose))
+    Ok(parse_quizzes(buffer, &topic.file_name, verbose))
 }
 
 // Leaderboards: Read and Write
@@ -177,13 +181,15 @@ pub fn parse_leaderboard(string: &str) -> Result<(String, String, u64, u64), Str
 /// Parses input from string and adds it to leaderboards
 fn parse_leaderboards(leaderboards: String, verbose: bool) -> leaderboards::Leaderboards {
     let mut ldbs = leaderboards::Leaderboards::new();
+    let mut curr_line = 1;
     for leaderboard in leaderboards.split('\n') {
         if !leaderboard.is_empty() {
             match parse_leaderboard(leaderboard) {
                 Ok(ldb) => ldbs.add_new_leaderboards(&ldb.0, &ldb.1, ldb.2, ldb.3),
-                Err(err) => if verbose { println!("Error in parsing lines in topics.csv: {err}") }
+                Err(err) => if verbose { println!("Error in parsing lines in leaderboards.csv on line {curr_line}: {err}") }
             }
         }
+        curr_line = curr_line + 1
     }
     ldbs
 }
--- src/opt.rs
+++ src/opt.rs
@@ -1,6 +1,7 @@
 use structopt::StructOpt;
 
-use crate::files;
+use crate::files::{self, load_topics};
+use crate::leaderboards;
 
 #[derive(StructOpt)]
 enum Command {
@@ -10,7 +11,8 @@ enum Command {
     },
     Leaderboards {
         topic: String
-    }
+    },
+    List,
 }
 
 #[derive(StructOpt)]
@@ -28,11 +30,42 @@ impl Opt {
             // TODO: P;ay Game and Use Data
             /// Topic is optional
             Command::Play { topic } => {
-                // let topics = files::load_topics(option.verbose)?;
-                // let leaderboards = files::load_leaderboards(option.verbose)?;
+
+                // If topic isn't given an argument:
+                // 1. List all Topics
+                // 2. Wait for input
+                // 3. On input, go to get_topic
+                // 4. If get_topic does not find the related topic, end
+                // 5. Else proceed to play
+
+                let topics = files::load_topics(option.verbose)?;
+                let mut leaderboards = files::load_leaderboards(option.verbose)?;
+
+                if topic == None{
+                    
+                }
+
+
                 Ok(())
             },
             Command::Leaderboards { topic } => {
+                let mut leaderboards = files::load_leaderboards(option.verbose)?;
+                match leaderboards.get_leaderboards_by_name(&topic){
+                    Some(t) => {
+                        for i in t{
+                            i.print();
+                        }
+                    },
+                    None => println!("No leaderboards for {topic} found"),
+                }
+                Ok(())
+            },
+            Command::List{} => {
+                let topics = files::load_topics(option.verbose)?;
+                // Lists all topics
+
+                topics.print_all_topics();
+
                 Ok(())
             }
         }
